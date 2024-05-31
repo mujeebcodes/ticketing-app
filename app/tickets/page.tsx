6b22090682c4c98a049647cacc0faac01010114b
mujeebcodes <mailmujeeb@icloud.com>
2024-05-31 23:15:59 +0100

    filtering and sorting functionality implemented

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -3,30 +3,55 @@ import DataTable from "./DataTable";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import Pagination from "@/components/Pagination";
+import StatusFilter from "@/components/StatusFilter";
+import { Status, Ticket } from "@prisma/client";
 
-interface SearchParams {
+export interface SearchParams {
   page: string;
+  status: Status;
+  orderBy: keyof Ticket;
 }
 
 const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
   const pageSize = 10;
 
   const page = parseInt(searchParams.page) || 1;
-  const ticketCount = await prisma.ticket.count();
+  const orderBy = searchParams.orderBy ? searchParams.orderBy : "createdAt";
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
 
+  let where = {};
+
+  if (status) {
+    where = { status };
+  } else {
+    where = {
+      NOT: [{ status: "CLOSED" as Status }],
+    };
+  }
+
+  const ticketCount = await prisma.ticket.count({ where });
   const tickets = await prisma.ticket.findMany({
+    where,
+    orderBy: { [orderBy]: "desc" },
     take: pageSize,
     skip: (page - 1) * pageSize,
   });
   return (
     <div>
-      <Link
-        href="/tickets/new"
-        className={buttonVariants({ variant: "default" })}
-      >
-        New Ticket
-      </Link>
-      <DataTable tickets={tickets} />
+      <div className="flex gap-2">
+        <Link
+          href="/tickets/new"
+          className={buttonVariants({ variant: "default" })}
+        >
+          New Ticket
+        </Link>
+
+        <StatusFilter />
+      </div>
+      <DataTable tickets={tickets} searchParams={searchParams} />
       <Pagination
         itemCount={ticketCount}
         pageSize={pageSize}
---
+++ components/StatusFilter.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+
+type Props = {};
+
+const statuses: { label: string; value?: string }[] = [
+  { label: "Open/Started" },
+  { label: "Open", value: "OPEN" },
+  { label: "Started", value: "STARTED" },
+  { label: "Closed", value: "CLOSED" },
+];
+const StatusFilter = (props: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const onChange = (status) => {
+    const params = new URLSearchParams();
+    if (status) params.append("status", status);
+
+    const query = params.size ? `?${params.toString()}` : "0";
+    router.push(`/tickets${query}`);
+  };
+  return (
+    <Select
+      defaultValue={searchParams.get("status") || ""}
+      onValueChange={onChange}
+    >
+      <SelectTrigger className="w-[200px]">
+        <SelectValue placeholder="Filter by Status ..." />
+      </SelectTrigger>
+
+      <SelectContent>
+        <SelectGroup>
+          {statuses.map((status) => (
+            <SelectItem key={status.value || "0"} value={status.value || "0"}>
+              {status.label}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
+export default StatusFilter;
